9d8a7520c338a0043b527016b034cfe0989e3d64
Marcin Grajdek <grajdek.m@vp.pl>
2022-07-17 14:11:38 +0200

    Add Components & param Children - orginal 15.5

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -6,9 +6,9 @@ const SearchForm = () => {
     return (
         <form className={styles.searchForm}>
             <TextInput placeholder="Search..." />
-            <TextInput placeholder="Add new column" />
-            <Button text="Search" />
-            <Button text="Create" />
+            <Button>
+                <span className="fa fa-search" />
+            </Button>
         </form>
     );
   };
