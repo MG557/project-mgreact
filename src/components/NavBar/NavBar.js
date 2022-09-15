87fe3740b810c62c1dc5ffe1ef5744ff8436e4b7
Marcin Grajdek <grajdek.m@vp.pl>
2022-09-15 09:08:30 +0200

    React Router - add NavLink 18.2

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,5 @@
 import styles from './NavBar.module.scss';
-
+import { NavLink } from 'react-router-dom';
 
 
 const NavBar = () => {
@@ -8,10 +8,10 @@ const NavBar = () => {
             <div className={styles.icon}>
                 <a href='/' className='fa fa-tasks'> </a>
                 <ul className={styles.links}>
-                    <li><a href='/'>Home</a></li>
-                    <li><a href='/favorite'>Favorite</a></li>
-                    <li><a href='/about'>About</a></li>
-                </ul>
+                    <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/" >Home</NavLink></li>
+                    <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/favorite" >Favorite</NavLink></li>
+                    <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/about" >About</NavLink></li>
+                </ul> 
             </div>
         </nav>
     )
